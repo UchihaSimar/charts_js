1510e17cf0b2d8399cae5addfc13c7f254b6872b
Gursimar Singh Chawla <guchawla@deloitte.com>
2020-07-27 17:32:30 +0530

    Port error

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var path = require('path');
 var app = express();
-const port = 3000
+const PORT = process.env.PORT || 5000
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname + '/ChartJS_Demo.html'));
